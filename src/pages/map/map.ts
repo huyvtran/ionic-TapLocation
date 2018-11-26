a033c272f0c8c7e8daa1d73947c6817b5f56fb18
Romeo292 <rngobeni168@gmail.com>
2018-11-26 15:25:59 +0200

    hi guys, you're nice.

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,25 +1,79 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+import leaflet from 'leaflet';
+import { NativeGeocoder, NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
+ 
 /**
  * Generated class for the MapPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
-@IonicPage()
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
 })
 export class MapPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('map') mapContainer: ElementRef;
+  map: any;
+  constructor(public navCtrl: NavController,private alertCtrl: AlertController,private nativeGeocoder: NativeGeocoder) {
+ 
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MapPage');
+ 
+  ionViewDidEnter() {
+    this.loadmap();
   }
+ 
+  loadmap() {
+    this.map = leaflet.map("map").fitWorld();
+    leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attributions: 'www.tphangout.com',
+      maxZoom: 100
+    }).addTo(this.map);
+  }
+ 
+  addMarker() {
+    let prompt = this.alertCtrl.create({
+      title: 'Add Marker',
+      message: "Enter location",
+      inputs: [
+        {
+          name: 'city',
+          placeholder: 'Pin a location'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            
+            this.geoCodeandAdd(data.city);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+ 
+  geoCodeandAdd(city) {
+    this.nativeGeocoder.forwardGeocode(city)
+      .then((coordinates: NativeGeocoderForwardResult[]) => {
+        let markerGroup = leaflet.featureGroup();
+      let marker: any = leaflet.marker([coordinates[0].latitude, coordinates[0].longitude]).on('click', () => {
+        alert('Marker clicked');
+      })
+      markerGroup.addLayer(marker);
+      this.map.addLayer(markerGroup);
+      })
+  .catch((error: any) => console.log(error));
+  }
+ 
 
 }
